3cc393315c4ccde21c3b35ced162af51f9756dee
TallBadgerfromTibet <tashmatov.13@icloud.com>
2022-05-30 16:27:01 +0600

    commit first

--- islam_tashmatov_hw_5/hw_5.py
+++ islam_tashmatov_hw_5/hw_5.py
@@ -1,64 +1,47 @@
-import re
+import os
+import random
+from envparse import env
 
-file_path4 = 'MOCK_DATA.txt'
-result_file_path4 = '../islam_tashmatov_hw_5/name.txt'
-file_reader4 = open(file_path4, mode="r", encoding="Latin-1")
-final_result4 = open(result_file_path4, mode="w", encoding="Latin-1")
+class Casino:
+    def __init__(self):
+        self.__cash = int(os.environ.get('MY_MONEY'))
 
-text = file_reader4.read()
+        while int(self.__cash) > 0:
+            print (f"Ваш денги: {self.__cash}")
+            self.__win_slot = random.randint(1, 30)
+            print(self.__win_slot)
+            self.__slot = int(30)
+            self.__bet = int(input('Сделайте ставку: '))
+            if self.__bet > self.__cash:
+                print("У вас недостаточно денег! ")
+            else:
+                self.__choice = int(input('Выберите слот: '))
+                if self.__choice > self.__slot:
+                    print("Выберите слота только от 1-30")
+                    userRound = input('Хотите ешё поиграть?("да" или "нет"')
+                    if userRound == 'да':
+                        continue
+                    elif userRound == 'нет':
+                        break
+                    else:
+                        print("да или нет?")
 
-s4 = r"[A-Z]+[A-z]+\w+\s+[A-z]+[a-z]+\w+|[A-Z]+[A-z]+\w+\s+[A-Z][']\s+[A-z]+[a-z]+\w+"
-results_all = re.findall(s4, text)
 
-for item in results_all:
-    final_result4.write(item+'\n')
-    print(f"total:{str(len(results_all))}")
+    @property
+    def win(self):
+        return self.__win_slot
 
-file_path3 = 'MOCK_DATA.txt'
-result_file_path3 = '../islam_tashmatov_hw_5/mail.txt'
+    @win.setter
+    def win(self, value):
+        self.__win_slot = value
 
-file_reader3 = open(file_path3, mode='r', encoding="UTF-8")
-final_results3 = open(result_file_path3, mode='w', encoding="UTF-8")
+    @property
+    def slot(self):
+        return self.__slot
 
-text3 = file_reader3.read()
+    @slot.setter
+    def slot(self, value):
+        self.__slot = value
 
-s3 = r'\w+@\w+.[a-z]+'
-results_all = re.findall(s3, text3)
 
-for item in results_all:
-    final_results3.write(item + '\n')
-print(f'Total:{str(len(results_all))}')
-
-# номера
-file_path2 = 'MOCK_DATA.txt'
-result_file_path2 = '../islam_tashmatov_hw_5/adresse.txt'
-
-file_reader2 = open(file_path2, mode='r', encoding="UTF-8")
-final_results2 = open(result_file_path2, mode='w', encoding="UTF-8")
-
-text2 = file_reader2.read()
-
-s2 = r'#\w+'
-results_all = re.findall(s2, text2)
-
-for item in results_all:
-    final_results2.write(item + '\n')
-print(f'Total:{str(len(results_all))}')
-
-# расширенная информация
-file_path1 = 'MOCK_DATA.txt'
-result_file_path1 = '../islam_tashmatov_hw_5/info.txt'
-
-file_reader1 = open(file_path1, mode='r', encoding="UTF-8")
-final_results1 = open(result_file_path1, mode='w', encoding="UTF-8")
-
-text1 = file_reader1.read()
-
-s1 = r"[A-Z]+[a-z]+\w+[.]+[a-z]+[0-9]|[A-Z]+[a-z]+\w+[.]+[a-z]+|[A-Z]+[a-z]+[.]+[a-z]+[0-9]|" \
-     r"[A-Z]+[a-z]+[.]+[a-z]+|[A-Z]+[a-z]+[.]+[a-z]+|[A-Z]+[.]+[a-z]+|[A-Z]+[.]+[a-z]+[0-9]" \
-
-results_all = re.findall(s1, text1)
-
-for item in results_all:
-    final_results1.write(item + '\n')
-print(f'Total:{str(len(results_all))}')
+env.read_envfile('setings.env')
---
+++ islam_tashmatov_hw_6/hw_6.py
@@ -0,0 +1,64 @@
+import re
+
+file_path4 = 'MOCK_DATA.txt'
+result_file_path4 = '../islam_tashmatov_hw_6/name.txt'
+file_reader4 = open(file_path4, mode="r", encoding="Latin-1")
+final_result4 = open(result_file_path4, mode="w", encoding="Latin-1")
+
+text = file_reader4.read()
+
+s4 = r"[A-Z]+[A-z]+\w+\s+[A-z]+[a-z]+\w+|[A-Z]+[A-z]+\w+\s+[A-Z][']\s+[A-z]+[a-z]+\w+"
+results_all = re.findall(s4, text)
+
+for item in results_all:
+    final_result4.write(item+'\n')
+    print(f"total:{str(len(results_all))}")
+
+file_path3 = 'MOCK_DATA.txt'
+result_file_path3 = '../islam_tashmatov_hw_6/mail.txt'
+
+file_reader3 = open(file_path3, mode='r', encoding="UTF-8")
+final_results3 = open(result_file_path3, mode='w', encoding="UTF-8")
+
+text3 = file_reader3.read()
+
+s3 = r'\w+@\w+.[a-z]+'
+results_all = re.findall(s3, text3)
+
+for item in results_all:
+    final_results3.write(item + '\n')
+print(f'Total:{str(len(results_all))}')
+
+# номера
+file_path2 = 'MOCK_DATA.txt'
+result_file_path2 = '../islam_tashmatov_hw_6/adresse.txt'
+
+file_reader2 = open(file_path2, mode='r', encoding="UTF-8")
+final_results2 = open(result_file_path2, mode='w', encoding="UTF-8")
+
+text2 = file_reader2.read()
+
+s2 = r'#\w+'
+results_all = re.findall(s2, text2)
+
+for item in results_all:
+    final_results2.write(item + '\n')
+print(f'Total:{str(len(results_all))}')
+
+# расширенная информация
+file_path1 = 'MOCK_DATA.txt'
+result_file_path1 = '../islam_tashmatov_hw_6/info.txt'
+
+file_reader1 = open(file_path1, mode='r', encoding="UTF-8")
+final_results1 = open(result_file_path1, mode='w', encoding="UTF-8")
+
+text1 = file_reader1.read()
+
+s1 = r"[A-Z]+[a-z]+\w+[.]+[a-z]+[0-9]|[A-Z]+[a-z]+\w+[.]+[a-z]+|[A-Z]+[a-z]+[.]+[a-z]+[0-9]|" \
+     r"[A-Z]+[a-z]+[.]+[a-z]+|[A-Z]+[a-z]+[.]+[a-z]+|[A-Z]+[.]+[a-z]+|[A-Z]+[.]+[a-z]+[0-9]" \
+
+results_all = re.findall(s1, text1)
+
+for item in results_all:
+    final_results1.write(item + '\n')
+print(f'Total:{str(len(results_all))}')
